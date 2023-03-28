f4de000f4d542f4cbdd5baaeb903fff01532fbac
Swaraj Kumar <leo.swaraj.5@gmail.com>
2023-03-28 08:51:53 +0530

    Initial Commit for Portfolio repository

---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss'],
+})
+export class BannerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    const options = {
+      strings: ['Software Developer.', 'MEAN Developer.', 'Full-Stack Developer.'],
+      typeSpeed: 150,
+      backSpeed: 150,
+      showCursor: true,
+      cursorChar: '|',
+      loop: true,
+    };
+
+    const typed = new Typed('.sec-text', options);
+  }
+}
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, HostListener  } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  navbarClass: string = 'navbar-bg-transparent'
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    // console.log(window.scrollY)
+    if (window.scrollY > 75) {
+      this.navbarClass = 'navbar-bg-black';
+    } else {
+      this.navbarClass = 'navbar-bg-transparent';
+    }
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
