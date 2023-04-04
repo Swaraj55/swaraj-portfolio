57e4b00618ad2e50cfb08d88ef426fc5e425a39d
Swaraj Kumar <leo.swaraj.5@gmail.com>
2023-04-04 10:47:36 +0530

    Experience section is mostly completed

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceComponent implements OnInit {
 
+  programmingLangSeceon: string[] = ['JavaScript', 'TypeScript', 'Angular 14+', 'Angular Material', 'HTML5', 'CSS3', 'Bootstrap 4 & 5', 'Ng-Gridster2', 'NodeJS'];
+
   constructor() { }
 
   ngOnInit(): void {
